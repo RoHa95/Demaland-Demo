51a2d1a5d24e250d85fbba94e14839bde741cae3
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-10-12 23:47:18 +0330

    completed static style of our services section

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -3,6 +3,10 @@ console.log("hi");
 const clickablemenu = document.querySelector(".clickable");
 const clickablelist = document.querySelector(".listmenu");
 const menu = document.querySelector(".menu");
+
+const servicesbtn = document.querySelector(".services");
+const serviceschild = document.querySelector(".services-child");
+
 // document.addEventListener("click", e=>{
 // console.log("uuu");
 
@@ -16,5 +20,13 @@ clickablelist.addEventListener("click",()=>{
 })
 menu.addEventListener("blur", ()=>{
     clickablelist.classList.add("hidden");
+    
+})
+
+servicesbtn.addEventListener("click",e=>{
+    console.log("servic");
+    servicesbtn.classList.add("bg-gray-100");
+    serviceschild.classList.remove("hidden");
+    serviceschild.classList.add("flex");
     
 })
