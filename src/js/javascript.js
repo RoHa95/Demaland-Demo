a8fae1ba39a4e1c23b3e7e83207fe7ab0462d35f
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-10-13 14:42:54 +0330

    finish our services section

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,32 +1,34 @@
 console.log("hi");
-
+//navbar section
 const clickablemenu = document.querySelector(".clickable");
 const clickablelist = document.querySelector(".listmenu");
 const menu = document.querySelector(".menu");
-
-const servicesbtn = document.querySelector(".services");
+// our services section
+const servicesbtn = document.querySelectorAll(".services");
 const serviceschild = document.querySelector(".services-child");
 
-// document.addEventListener("click", e=>{
-// console.log("uuu");
+//navbar section functions
+clickablemenu.addEventListener("click", (e) => {
+  clickablelist.classList.toggle("hidden");
+});
+clickablelist.addEventListener("click", () => {
+  console.log("click");
+});
+menu.addEventListener("blur", () => {
+  clickablelist.classList.add("hidden");
+});
 
-// })
-clickablemenu.addEventListener("click",e=>{
-    clickablelist.classList.toggle("hidden");
-})
-clickablelist.addEventListener("click",()=>{
-    console.log("click");
-    
-})
-menu.addEventListener("blur", ()=>{
-    clickablelist.classList.add("hidden");
-    
-})
-
-servicesbtn.addEventListener("click",e=>{
+// our services section functions
+servicesbtn.forEach((item) => {
+  item.addEventListener("click", (e) => {
     console.log("servic");
-    servicesbtn.classList.add("bg-gray-100");
-    serviceschild.classList.remove("hidden");
-    serviceschild.classList.add("flex");
-    
-})
+    item.classList.add("bg-gray-100");
+    item.querySelector(".services-child").classList.remove("hidden");
+    item.querySelector(".services-child").classList.add("flex");
+  });
+  item.addEventListener("blur", (e) => {
+    item.classList.remove("bg-gray-100");
+    item.querySelector(".services-child").classList.add("hidden");
+    item.querySelector(".services-child").classList.remove("flex");
+  });
+});
