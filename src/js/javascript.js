247f21c159681a1dc88026443cb0d853ebd474ba
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-10-14 19:42:53 +0330

    completed our performance section

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -7,6 +7,9 @@ const menu = document.querySelector(".menu");
 const servicesbtn = document.querySelectorAll(".services");
 const serviceschild = document.querySelector(".services-child");
 
+//our performance
+const performancediv = document.querySelectorAll(".performance");
+
 //navbar section functions
 clickablemenu.addEventListener("click", (e) => {
   clickablelist.classList.toggle("hidden");
@@ -22,13 +25,32 @@ menu.addEventListener("blur", () => {
 servicesbtn.forEach((item) => {
   item.addEventListener("click", (e) => {
     console.log("servic");
+    servicesbtn.forEach((item) => {
+      item.classList.remove("bg-gray-100");
+      item.querySelector(".services-child").classList.add("hidden");
+      item.querySelector(".services-child").classList.remove("flex");
+    });
     item.classList.add("bg-gray-100");
     item.querySelector(".services-child").classList.remove("hidden");
     item.querySelector(".services-child").classList.add("flex");
   });
-  item.addEventListener("blur", (e) => {
-    item.classList.remove("bg-gray-100");
-    item.querySelector(".services-child").classList.add("hidden");
-    item.querySelector(".services-child").classList.remove("flex");
+  // item.addEventListener("blur", (e) => {
+  //   item.classList.remove("bg-gray-100");
+  //   item.querySelector(".services-child").classList.add("hidden");
+  //   item.querySelector(".services-child").classList.remove("flex");
+  // });
+});
+
+// our performance section functions
+performancediv.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    performancediv.forEach((item) => {
+      item.querySelector(".performance-tag").classList.add("hidden");
+      item.querySelector(".title-performance").classList.add("text-lg");
+      item.querySelector(".title-performance").classList.remove("text-xl");
+    });
+    item.querySelector(".performance-tag").classList.remove("hidden");
+    item.querySelector(".title-performance").classList.remove("text-lg");
+    item.querySelector(".title-performance").classList.add("text-xl");
   });
 });
